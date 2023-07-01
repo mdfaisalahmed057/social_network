f6963a013f69a52db9b3a53fa5653f50989896ad
mdfaisalahmed057 <mdfais2995@gmail.com>
2023-07-01 22:37:11 +0530

    packges

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
- 
   return {
-    presets:['react-native-reanimated/plugin','@babel/preset-env','@babel/preset-react'],
-    plugins: [
-      "@babel/plugin-transform-export-namespace-from",
-      'react-native-reanimated/plugin',
-  ],
-   };
+    presets: ['babel-preset-expo'],
+  };
 };
